bcab015a1d1b592be30b0a8b375f881f8e2ecb99
zonaren <sondre.torgersen@gmail.com>
2025-02-19 22:13:27 +0100

    Fix match point calculation logic for score updates in tie scenarios

--- score-logics.js
+++ score-logics.js
@@ -12,16 +12,16 @@ function updateTotalScores(p1id, p2id, p1Score, p2Score) {
     if (player1Score >= 21 && player2Score < 11) {
         player1.matchPoints += 2;
         player2.matchPoints += 0;
-    } else if (player1Score >= 21 && player2Score < 21 && player2Score >= 11) {
+    } else if (player1Score >= 21 && player2Score >= 11 && player1Score > player2Score) {
         player1.matchPoints += 2;
         player2.matchPoints += 1;
     } else if (player2Score >= 21 && player1Score < 11) {
         player1.matchPoints += 0;
         player2.matchPoints += 2;
-    } else if (player2Score >= 21 && player1Score < 21 && player1Score >= 11) {
+    } else if (player2Score >= 21 && player1Score >= 11 && player2Score > player1Score) {
         player1.matchPoints += 1;
         player2.matchPoints += 2;
-    } else if (player1Score >= 21 && player2Score >= 21) {
+    } else if (player1Score >= 21 && player2Score >= 21 && player1Score === player2Score) {
         player1.matchPoints += 1.5;
         player2.matchPoints += 1.5;
     }
