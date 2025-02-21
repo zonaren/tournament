05c702628431cf7e88797a512b29212a66bca88c
zonaren <sondre.torgersen@gmail.com>
2025-02-22 00:09:13 +0100

    Add walkover player in createMatchSetup function for odd player counts

--- generateMatches.js
+++ generateMatches.js
@@ -3,6 +3,7 @@ function createMatchSetup(totalPlayers, totalRounds, players) {
         console.log(`Total players is an odd number`,`(`,totalPlayers,`)`);
         totalPlayers = totalPlayers +1;
         console.log(`Startnumber `,totalPlayers, ` is set to be Walkover`)
+        addNewPlayer('Walkover');
     }
 
     const totalCourts = totalPlayers / 2;
