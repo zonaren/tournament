142ad1a55d4adde647a54e27c13ed1109b97f0f6
zonaren <sondre.torgersen@gmail.com>
2025-03-10 22:06:35 +0100

    Remove unused warning script and clean up index.html

--- warning.js
+++
@@ -1,9 +0,0 @@
-// Description: This script is used to show a warning message when the user tries to leave the page.
-// This scipt is temporary and will be removed in the future.
-
-window.addEventListener('beforeunload', function (e) {
-var confirmationMessage = 'Are you sure you want to leave this page?';
-    
-e.returnValue = confirmationMessage; 
-return confirmationMessage;
-});
