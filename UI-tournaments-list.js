a28db5266f6a181dea0e30660f6068f584d3aedb
zonaren <sondre.torgersen@gmail.com>
2025-05-02 12:53:55 +0200

    Fix: set current tournament before loading players and display overview

--- UI-tournaments-list.js
+++ UI-tournaments-list.js
@@ -67,14 +67,15 @@ export function displayTournamentsList() {
 function loadTournament(id) {
     const tournament = Tournaments.get(id);
     if (tournament) {
-        console.log('Load tournament:', tournament);
+        Tournaments.setCurrentTournament(tournament.id);
+        console.log('Load tournament:', tournament, 'id: ', tournament.id);
         Players.loadPlayersFromTournament(tournament.getPlayers());
         const tournamentsList = document.getElementById('tournamentsList');
         tournamentsList.remove();
         displayTournamentOverview(tournament);
         const header = document.getElementById('header');
         header.remove();
-        Tournaments.setCurrentTournament(tournament.id);
+        
     }
 }
 
