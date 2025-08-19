2d70b5d0965fff9caba8426a9f8105f11aa6c52d
zonaren <sondre.torgersen@gmail.com>
2025-08-19 15:22:23 +0200

    Update version number to 0.0.5a

--- UI/UI-main.js
+++ UI/UI-main.js
@@ -5,7 +5,7 @@ header.className = 'header';
 
 const versionText = document.createElement('p');
 versionText.id = 'versionText';
-versionText.textContent = '0.0.5 (19. aug 2025)';
+versionText.textContent = '0.0.5a (19. aug 2025)';
 header.appendChild(versionText);
 
 const titleText = document.createElement('h2');
